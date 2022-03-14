b1ba452f401dbd02d9a8c19dcd22530a1e5ddc3e
sridharandharani <sridharandharani1815@gmail.com>
2022-03-14 12:45:34 +0530

    ifelse completed

---
+++ src/Ifelse.java
@@ -0,0 +1,12 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        Integer a = 18;
+        if (a < 20)
+        {
+            System.out.println("It is less than 20");
+        }
+        else{
+            System.out.println("it is greater than 20 ");
+        }
+    }
+}
---
+++ src/oddoreven.java
@@ -0,0 +1,12 @@
+public class oddoreven {
+    public static void main(String[] args) {
+        Integer a = 9;
+        if (a % 2 == 0)
+        {
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
